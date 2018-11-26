c1212c3bf67ccc1c9e212ea358e6e35f5e03a93a
Lucas Sousa de Freitas <lucas.sousa@alumni.mondragon.edu>
2018-11-26 17:09:56 +0100

    Fix typo

--- src/test/java/com/zazpi/nozama/NozamaApplicationTest.java
+++ src/test/java/com/zazpi/nozama/NozamaApplicationTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class NozamaApplicationTests {
+public class NozamaApplicationTest {
 
 	@Test
 	public void sampleTest() {
