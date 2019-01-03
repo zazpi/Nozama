bfacd7cb684649215f3dd211534e33123c1f091b
hodeiab03 <hodei.aguirre@alumni.mondragon.edu>
2019-01-03 10:04:19 +0100

    [#31] Change database schema

--- app/src/main/resources/db/migration/V1__Structure_Client.sql
+++ app/src/main/resources/db/migration/V1__Structure_Client.sql
@@ -6,62 +6,74 @@ CREATE TABLE orders (
  departureDate   DATE
 );
 
-CREATE TABLE ordersProducts (
- orderID         SERIAL,
- productModelID  SERIAL
-);
-
 CREATE TABLE productModel (
  productModelID  SERIAL,
  name            VARCHAR(20),
  description     VARCHAR(250),
  weight          INT,
- x		 INT,
+ x		           INT,
  y               INT,
  z               INT
+);
+
+CREATE TABLE ordersProducts (
+ orderID         INT,
+ productModelID  INT
+);
+
+CREATE TABLE shelf (
+ shelfID         SERIAL,
+ capacity        INT,
+ position        INT,
+ warehouseID     INT
+);
+
+CREATE TABLE productsLocation (
+  orderID         INT,
+  productModelID  INT,
+  shelfID         INT,
+  warehouseID     INT
 );
 
 CREATE TABLE productStack (
  productID       SERIAL,
  stock           INT,
- startDate       DATE,
- endDate         DATE,
  productModelID  INT,
- shelvesID       INT
-);
-
-CREATE TABLE shelves (
- shelvesID       SERIAL,
- capacity        INT,
- position        INT,
- warehouseID    INT
+ shelfID         INT,
+ warehouseID     INT
 );
 
 CREATE TABLE warehouse (
  warehouseID     SERIAL,
- nameWarehouse   VARCHAR(20),
- location        INT,
- warehouseSize   INT
+ name            VARCHAR(20),
+ location        INT
 );
 
 ALTER TABLE orders
-ADD CONSTRAINT ORDER_PK PRIMARY KEY (orderID);
-
-ALTER TABLE warehouse
-ADD CONSTRAINT WAREHOUSE_PK PRIMARY KEY (warehouseID);
+  ADD CONSTRAINT ORDER_PK PRIMARY KEY (orderID);
 
 ALTER TABLE productModel
-ADD CONSTRAINT PRODUCTMODEL_PK PRIMARY KEY (productModelID);
-
-ALTER TABLE shelves
-ADD CONSTRAINT SHELVES_PK PRIMARY KEY (shelvesID),
-ADD CONSTRAINT SHELVES_WAREHOUSE_FK FOREIGN KEY (warehouseID) REFERENCES warehouse (warehouseID);
+  ADD CONSTRAINT PRODUCTMODEL_PK PRIMARY KEY (productModelID);
 
 ALTER TABLE ordersProducts
-ADD CONSTRAINT ORDERSPRODUCTS_ORDER_FK FOREIGN KEY (orderID) REFERENCES orders (orderID),
-ADD CONSTRAINT ORDERSPRODUCTS_PRODUCTMODEL_FK FOREIGN KEY (productModelID) REFERENCES productModel (productModelID);
+  ADD CONSTRAINT ORDERSPRODUCTS_ORDER_FK FOREIGN KEY (orderID) REFERENCES orders (orderID),
+  ADD CONSTRAINT ORDERSPRODUCTS_PRODUCTMODEL_FK FOREIGN KEY (productModelID) REFERENCES productModel (productModelID),
+  ADD CONSTRAINT ORDERSPRODUCTS_PK PRIMARY KEY (orderID, productModelID);
+
+ALTER TABLE warehouse
+  ADD CONSTRAINT WAREHOUSE_PK PRIMARY KEY (warehouseID);
+
+ALTER TABLE shelf
+  ADD CONSTRAINT SHELF_WAREHOUSE_FK FOREIGN KEY (warehouseID) REFERENCES warehouse (warehouseID),
+  ADD CONSTRAINT SHELF_PK PRIMARY KEY (shelfID, warehouseID);
+
+ALTER TABLE productsLocation
+  ADD CONSTRAINT PRODUCTSLOCATION_ORDER_FK FOREIGN KEY (orderID) REFERENCES orders (orderID),
+  ADD CONSTRAINT PRODUCTSLOCATION_PRODUCTMODEL_FK FOREIGN KEY (productModelID) REFERENCES productModel (productModelID),
+  ADD CONSTRAINT PRODUCTSLOCATION_SHELF_FK FOREIGN KEY (shelfID, warehouseID) REFERENCES shelf (shelfID, warehouseID),
+  ADD CONSTRAINT PRODUCTSLOCATION_PK PRIMARY KEY (orderID, productModelID, shelfID, warehouseID);
 
 ALTER TABLE productStack
-ADD CONSTRAINT PRODUCTSTACK_PK PRIMARY KEY (productID, startDate, endDate),
-ADD CONSTRAINT PRODUCTSTACK_PRODUCTMODEL_FK FOREIGN KEY (productModelID) REFERENCES productModel (productModelID),
-ADD CONSTRAINT PRODUCTSTACK_SHELVES_FK FOREIGN KEY (shelvesID) REFERENCES shelves (shelvesID);
+  ADD CONSTRAINT PRODUCTSTACK_PK PRIMARY KEY (productID),
+  ADD CONSTRAINT PRODUCTSTACK_PRODUCTMODEL_FK FOREIGN KEY (productModelID) REFERENCES productModel (productModelID),
+  ADD CONSTRAINT PRODUCTSTACK_SHELF_FK FOREIGN KEY (shelfID, warehouseID) REFERENCES shelf (shelfID, warehouseID);
