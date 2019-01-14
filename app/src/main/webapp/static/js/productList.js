62570cdbe60ac68a539a3a4f2e60e293c715bd7d
sousadf <lucas.sousa@alumni.mondragon.edu>
2019-01-14 12:24:46 +0100

    [#49] Add edit functionality to product table.
    Added functions to delete, edit and add products. But it still needs
    backend to be 100% functional.

--- app/src/main/webapp/static/js/productList.js
+++ app/src/main/webapp/static/js/productList.js
@@ -1,20 +1,57 @@
-const productsTable = new Tabulator("#product-list-table", {
-    height: 400,
-    ajaxURL: "/api/product/list",
-    layout: "fitColumns",
-    tooltips: true,
-    addRowPos: "top",
-    history: true,
-    pagination: "local",
-    paginationSize: 15,
-    layoutColumnsOnNewData: true,
-    columns: [
-        {title: "ID", field: "id", align: "center", width: 100},
-        {title: "Name", field: "name",},
-        {title: "Description", field: "description"},
-        {title: "Stock", field: "stock"}
-    ],
-    /*rowClick:function(e, row){ //trigger an alert message when the row is clicked
-        alert("Row " + row.getData().id + " Clicked!!!!");
-    },*/
+$(document).ready(() => {
+    const productsTable = new Tabulator("#product-list-table", {
+        height: 400,
+        ajaxURL: "/api/product/list",
+        layout: "fitColumns",
+        selectable: true,
+        tooltips: true,
+        addRowPos: "top",
+        history: true,
+        pagination: "local",
+        paginationSize: 15,
+        layoutColumnsOnNewData: true,
+        columns: [
+            {title: "ID", field: "id", align: "center", width: 100},
+            {title: "Name", field: "name",},
+            {title: "Description", field: "description"},
+            {title: "Stock", field: "stock"}
+        ]
+    });
+
+    // FIXME: The below api requests can be used once backend functionality is correctly functional.
+    /*$("#delete-btn").click(async () => {
+        const deleteUrl = "/api/product/delete";
+        const data = productsTable.getSelectedData();
+        let ids = data.map((data) => {
+           return data.id;
+        });
+        const params = {
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json; charset=UTF-8"
+            },
+            body: JSON.stringify({productIds: ids}),
+            method: "POST"
+        };
+
+        await fetch(deleteUrl, params);
+    });*/
+
+    /*$("#edit").click(async () => {
+        const preloadUrl = "/api/product/preload";
+        const data = productsTable.getSelectedData();
+        console.log(data);
+        let id = data[0].id;
+        const params = {
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json; charset=UTF-8"
+            },
+            body: JSON.stringify({productId: id}),
+            method: "POST"
+        };
+        await fetch(preloadUrl, params);
+    });*/
 });
+
+
