f8c3566cb065ee4460375d2c0a477fd4f88ab684
Asier <asier.delanatividad@alumni.mondragon.edu>
2019-01-17 15:52:08 +0100

    [#46] Upgrade dashboard display

--- app/src/main/webapp/WEB-INF/tiles/view/body/dashboard.jsp
+++ app/src/main/webapp/WEB-INF/tiles/view/body/dashboard.jsp
@@ -1,17 +1,53 @@
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <link rel="stylesheet" href="/static/css/dashboard.css">
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
+<fmt:bundle basename="resources.View">
 <section class="dashboard">
-	<div id="timeseries"></div>
-	<div id="heatmap"></div>
-	<div id="map"></div>
+	<div id="map-heatmap">
+		<div id="heatmapGraph">
+			<header>
+	            <h2><fmt:message key="dashboard.orderPeriod"/></h2>
+	        </header>
+	        <div id="heatmap" ></div>
+		</div>
+		<div id="mapGraph">
+			<header>
+	            <h2><fmt:message key="dashboard.orderDestination"/></h2>
+	        </header>
+	        <div id="map"></div>
+		</div>
+	</div>
+	<div id="gauges">
+		<div id="gaugeGraph1">
+			<header>
+	            <h2><fmt:message key="dashboard.generalGauge"/></h2>
+	        </header>
+	    	<div id="gauge0" ></div>
+		</div>
+		<div id="gaugeGraph2">
+			<header>
+	            <h2><fmt:message key="dashboard.warehouse1Gauge"/></h2>
+	        </header>
+	        <div id="gauge1" ></div>
+		</div>
+		<div id="gaugeGraph3">
+			<header>
+	            <h2><fmt:message key="dashboard.warehouse2Gauge"/></h2>
+	        </header>
+	        <div id="gauge2" ></div>
+		</div>
+	</div>
 </section>
+</fmt:bundle>
+<script src="https://code.highcharts.com/highcharts.js"></script>
+<script src="https://code.highcharts.com/highcharts-more.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://code.highcharts.com/stock/highstock.js"></script>
 <script src="http://code.highcharts.com/maps/modules/map.js"></script>
 <script src="https://code.highcharts.com/stock/modules/exporting.js"></script>
 <script src="https://code.highcharts.com/mapdata/countries/es/es-all.js"></script>
 <script src="https://code.highcharts.com/modules/data.js"></script>
 <script src="https://code.highcharts.com/modules/heatmap.js"></script>
+<script src="https://code.highcharts.com/modules/solid-gauge.js"></script>
 
 <script src="/static/js/dashboard.js"></script>
--- app/src/main/webapp/static/js/dashboard.js
+++ app/src/main/webapp/static/js/dashboard.js
@@ -3,6 +3,19 @@ $(document).ready(function(){
 	$.getJSON('/api/order/list-day-month',    function (data) {
 		drawHeatMap(data);
 	});
+	//Map
+	$.getJSON('/api/order/list-location',    function (data) {
+		createMap(data);
+	});
+	
+	const warehouseIds = [0,1,2];
+	$.each(warehouseIds, (index, id) => {
+		$.getJSON('/api/stock/getcapacity?warehouseId=' + id, (data) => {
+			createGauge([data],'gauge' + id);
+		});
+	});
+	//Gauge
+
 });
 
 function drawHeatMap(data){
@@ -14,7 +27,7 @@ function drawHeatMap(data){
 		    plotBorderWidth: 1
 		  },
 		  title: {
-		    text: 'Orders per month per weekday'
+		    text: ''
 		  },
 		  xAxis: {
 		    categories: ['null','January', 'Febrary', 'March', 'April', 'May', 'June',
@@ -54,4 +67,100 @@ function drawHeatMap(data){
 		  }]
 
 		});	
+}
+
+//Map
+function createMap(data){
+	Highcharts.mapChart('map', {
+		 chart: {
+		     map: 'countries/es/es-all'
+		 },
+		 mapNavigation: {
+		     enabled: true,
+		     buttonOptions: {
+		         verticalAlign: 'bottom'
+		     }
+		 },
+		 title: {
+		   text: ''
+		 },
+		 colorAxis: {
+		     min: 0
+		 },
+		 series: [{
+		     data: data,
+		     name: 'Number of orders',
+		     states: {
+		         hover: {
+		             color: '#BADA55'
+		         }
+		     }
+		 }]
+		});
+}
+
+// Gauges
+function createGauge(data,container){
+	Highcharts.chart(container,{
+	    chart: {
+	        type: 'solidgauge'
+	    },
+	    title: null,
+	    pane: {
+	        center: ['50%', '60%'],
+	        size: '90%',
+	        startAngle: -90,
+	        endAngle: 90,
+	        background: {
+	            backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || '#EEE',
+	            innerRadius: '60%',
+	            outerRadius: '100%',
+	            shape: 'arc'
+	        }
+	    },
+	    tooltip: {
+	        enabled: false
+	    },
+	    // the value axis
+	    yAxis: {
+	        stops: [
+	            [0.1, '#55BF3B'], // green
+	            [0.5, '#DDDF0D'], // yellow
+	            [0.9, '#DF5353'] // red
+	        ],
+	        lineWidth: 0,
+	        minorTickInterval: null,
+	        tickAmount: 2,
+	        title: {
+	            y: -70
+	        },
+	        labels: {
+	            y: 16
+	        },
+	        min: 0,
+	        max: 100,
+	    },
+	    plotOptions: {
+	        solidgauge: {
+	            dataLabels: {
+	                y: 5,
+	                borderWidth: 0,
+	                useHTML: true
+	            }
+	        }
+	    },
+	    series: [{
+	        name: 'RPM',
+	        data: data,
+	        dataLabels: {
+	            format: '<div style="text-align:center"><span style="font-size:50px;color:' +
+	                ((Highcharts.theme && Highcharts.theme.contrastTextColor) || '#2f3542') + '">{y:.1f}%</span><br/>' +
+	                   '<span style="font-size:12px;color:silver">Ocupation</span></div>'
+	        },
+	        tooltip: {
+	            valueSuffix: ' revolutions/min'
+	        }
+	    }]
+
+	});
 }
