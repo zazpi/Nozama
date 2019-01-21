8893f250ffccc6223438e7e0b88a09913a4f60a8
Bingen Galartza <bingen.galartza@alumni.mondragon.edu>
2019-01-21 09:16:11 +0100

    [#46] Fix graph endpoint

--- app/src/main/java/com/zazpi/nozama/controller/OrderController.java
+++ app/src/main/java/com/zazpi/nozama/controller/OrderController.java
@@ -9,7 +9,6 @@ import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
