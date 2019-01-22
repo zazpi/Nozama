d7645ea590fb6e68d171c90365311cd2e5f680c9
sousadf <lucas.sousa@alumni.mondragon.edu>
2019-01-22 02:06:11 +0100

    [#45] Fix locale as it conflicts with other page

--- app/src/main/webapp/static/js/locale.js
+++ app/src/main/webapp/static/js/locale.js
@@ -3,7 +3,7 @@ $(document).ready(() => {
         const selectedOption = $('#locales').val();
         if (selectedOption !== ''){
             let page = window.location.protocol + "//" + window.location.host + window.location.pathname;
-            if(window.location.href.includes("product")){
+            if(window.location.href.includes("product?")){
                 const currentPage = window.location.href + "&lang=es";
                 const params = currentPage.split("?")[1];
                 const rParam = params.split("&")[0];
